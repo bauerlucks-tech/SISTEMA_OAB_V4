3bdd54854e33a59243f7c12fe1f898efa61e5d9f
Bauer <bauerlucks@gmail.com>
2026-02-09 17:42:44 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,23 @@
-
 from flask import Flask
 from .config import Config
 from .database import db
 
+
 def create_app():
-    app = Flask(__name__, static_folder="../static", template_folder="../templates")
+    app = Flask(__name__)
+
+    # Configurações
     app.config.from_object(Config)
 
+    # Banco de dados
     db.init_app(app)
 
-    with app.app_context():
-        db.create_all()
-
+    # Importa e registra rotas
     from .routes import main
     app.register_blueprint(main)
+
+    # Cria tabelas (se necessário)
+    with app.app_context():
+        db.create_all()
 
     return app
